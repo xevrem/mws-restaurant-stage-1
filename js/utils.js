2cb7db169281160935a6504bcda85660d15cdd54
erika jonell <erika.jonell@gmail.com>
2018-05-28 11:00:43 -0400

    fix: forgot to add utils.js
    adding utils.js to source control

---
+++ js/utils.js
@@ -0,0 +1,34 @@
+
+class Utils{
+  /*
+  *  Constructs a picture element with the following structure
+  *  <picture alt=''>
+  *    <source srcset="/img/1.jpg" media="(min-width: 601px)">
+  *    <source srcset="/img/1_md.jpg" media="(min-width: 401px)">
+  *    <img src="/img/1_sm.jpg" alt="restaurant">
+  *  </picture>
+  */
+  static assemblePictureHtml(picture, restaurant){
+    let imageName = DBHelper.imageUrlForRestaurant(restaurant);
+    
+    let lg = document.createElement('source');
+    lg.setAttribute('srcset', imageName);
+    lg.setAttribute('media', '(min-width: 601px)');
+    picture.append(lg);
+
+    let mdName = imageName.split('.jpg')[0] + '_md.jpg';
+    let md = document.createElement('source');
+    md.setAttribute('srcset', mdName);
+    md.setAttribute('media', '(min-width: 401px)');
+    picture.append(md);
+
+    let smName = imageName.split('.jpg')[0] + '_sm.jpg';
+    let img = document.createElement('img');
+    img.className = 'restaurant-img';
+    img.src = smName;
+    img.setAttribute('alt', restaurant.name);
+    picture.append(img);
+
+    return picture;
+  }
+}
