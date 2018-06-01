04bc66bb4e283416f8aced78e64a9f3a3cee5a5a
erika jonell <erika.jonell@gmail.com>
2018-06-01 12:08:04 -0400

    styling: implemented css-grid styling
    implemented css-grid styling for larger screen sizes
    
    --updated css to work with new syling
    --updated html to enable css-grid to differentiate specific elements on
    specific pages
    --updated main.js to apply needed classes to list items

--- js/main.js
+++ js/main.js
@@ -187,6 +187,7 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
  */
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
+  li.className = 'list-item';
 
   // const image = document.createElement('img');
   // image.className = 'restaurant-img';
